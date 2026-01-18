9cb10fe94e4cfc23bc621adc732418ba21ef7d0a
vaibhhaav <vaibhavgupta2021@gmail.com>
2026-01-18 23:38:13 +0530

    Date: 18/01/2026

---
+++ GeorgeAndAccommodation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GeorgeAndAccommodation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if (q - p >= 2) count++;
+        }
+        System.out.println(count);
+    }
+}
